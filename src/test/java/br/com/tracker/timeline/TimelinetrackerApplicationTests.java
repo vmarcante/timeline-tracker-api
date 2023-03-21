5b6a83805b4af353de1a0f86044ed7f331e01ef6
Vinicius <vinicius.marcante@kron.digital>
2023-03-21 19:43:59 -0300

    First Commit

---
+++ src/test/java/br/com/tracker/timeline/TimelinetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tracker.timeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelinetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
